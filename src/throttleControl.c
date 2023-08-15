0d45f1f3cb1eb5f44502adb3d7448d4eeeababfc
Voluruund <majidalessio@gmail.com>
2023-08-15 18:28:29 +0200

    Crash accelleratore facoltativo

--- src/throttleControl.c
+++ src/throttleControl.c
@@ -11,6 +11,7 @@
 #include <fcntl.h>
 #include <signal.h>
 #include <time.h>
+#include <limits.h>
 
 #include "definitions.h"
 #include "TC.h"
@@ -20,6 +21,7 @@ int sock, logTC;
 char buffer[100], toLog[60];
 
 void termHandlerTC(int);
+int throttleCrash();
 
 void mainThrottleControl(){
     char toAppend[1];
@@ -38,6 +40,10 @@ void mainThrottleControl(){
         memset(buffer, 0, sizeof buffer);
         memset(toAppend, 0, sizeof toAppend);
         readLine(sock, buffer);
+        if(throttleCrash()){
+            kill(getppid(), SIGUSR1);
+            break;
+        }
         int i = 11;
         while(buffer[i] != '\0'){
             sprintf(toAppend, "%c", buffer[i]);
@@ -49,6 +55,18 @@ void mainThrottleControl(){
     }
 
     termHandlerTC(0);
+}
+
+int throttleCrash(){
+    unsigned int randomNumber = 0;
+    for (int i = 0; i < 4; ++i) {
+        // Genera un numero randomico di 2 byte e shifta a sx per far spazio al successivo
+        unsigned int randomByte = rand() & 0xFF;
+        randomNumber = (randomNumber << 8) | randomByte;
+    }
+    // rangeAccepted ha un valore di 42949.67295 esattamente 1/10^5 rispetto a randomNumber che può valere fino a 4,294,967,295
+    double rangeAccepted =  (double)UINT_MAX / pow(10,5);
+    return randomNumber < rangeAccepted;
 }
 
 void termHandlerTC(int sig){
